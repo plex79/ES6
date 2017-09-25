4b6069214f94f190904e3182d04cf473d469f816
BOSS-PC\boss <contact@eplex.eu>
2017-09-25 13:55:46 +0200

    v3

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Gallery from './Gallery';
 
 class Global extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			query: ''
+			query: '',
+			items: []
 		}
 	}
 
@@ -14,8 +16,11 @@ class Global extends Component {
 		const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
 		fetch(`${BASE_URL}${this.state.query}`, { method: 'GET'})
 			.then(response => response.json())
-			.then(json => console.log(json));
-		console.log('search', this.state.query);
+			.then(json => {
+				let {items} = json;
+				this.setState({items})
+			});
+		
 	}
 
 	render() {
@@ -39,6 +44,9 @@ class Global extends Component {
 						</InputGroup.Addon>
 					</InputGroup>
 				</FormGroup>
+
+				<Gallery items={this.state.items} />
+
 			</div>
 		)
 	}
