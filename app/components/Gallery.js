50185968bf1a0bf0ee4af7f3c1215fdbb2085d82
BOSS-PC\boss <contact@eplex.eu>
2017-09-26 09:43:42 +0200

    final

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -2,13 +2,29 @@ import React, {Component} from 'react';
 
 class Gallery extends Component {
 	render() {
+
+		let alternate = 'photo.png';
+
 		return (
 			<div>
 			{
 				this.props.items.map((item, index) => {
-					let {title} = item.volumeInfo;
+					let {title, imageLinks, infoLink} = item.volumeInfo;
 					return (
-						<div key={index}>{title}</div>
+						<a 
+						 key={index} 
+						 className="book"
+						 href={infoLink}
+						 target="_blank"
+						 >
+							<img src={imageLinks !== undefined ? imageLinks.thumbnail : alternate} 
+							alt="book image"
+							className="book-img"
+							/>
+							<div className="book-text">
+								{title}
+							</div>
+						</a>
 					)
 				})
 			}
